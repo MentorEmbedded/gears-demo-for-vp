b44be7b375fa618b6293b5b5c88ea078406f89de
Ricardo Anguiano <ricardo_anguiano@mentor.com>
2014-01-15 07:37:41 -0800

    Added testing portion wrapped in ifdef.

--- main.c
+++ main.c
@@ -30,8 +30,119 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     mb_core_message("*** ARM A9 OpenGL Platform ***");
 #endif
 
+#ifndef TEST
     init_lcd();
   
     ui_loop();
+#else
+    // emit beginning of testing message
+    mb_core_message("---begin testing---");
+    #include "examples/gearsdrawmath.h"
+
+    // test setup
+    GLfloat actual_output[10], return_value;
+    #define FAIL 0
+    #define PASS 1
+    int test_result = FAIL;
+
+    // ------------------------------------------------------------------------
+    // test 1 setup
+    GLfloat input1[10] = {0.0, 1.0, 2.0, 3.0, 4.5, 5.9, -1.1, -2.3, -3.0, -4.4};
+    GLfloat expected_output1[10] = {0.0, 1.0, 2.0, 3.0, 4.5, 5.9, -1.1, -2.3, -3.0, -4.4};
+    test_result = FAIL;
+    
+    // run tests
+    for (int i = 0; i < 10; i++) {
+      return_value = gearsDrawMathIdentity(input1[i]);
+      actual_output[i] = return_value;
+    }
+
+    // compare values
+    for (int i = 0; i < 10; i++) {
+      if (expected_output1[i] == actual_output[i]) {
+	test_result = PASS;
+      }
+      else {
+	test_result = FAIL;
+	break;
+      }
+    }
+    
+    // emit result
+    if (test_result == FAIL)
+      mb_core_message("---unit test gearsDrawMathIdentity failed---");
+    if (test_result == PASS)
+      mb_core_message("---unit test gearsDrawMathIdentity passed---");
+
+    // ------------------------------------------------------------------------
+    // test 2 setup
+    GLfloat input2[10] = {0.0, 1.0, 2.0, 3.0, 4.5, 5.9, -1.1, -2.3, -3.0, -4.4};
+    GLfloat expected_output2[10] = {-9, -11, -13, -15, -18, -20.7999992, -6.80000019, -4.4000001, -3, -0.199999809};
+    // initialize test result
+    test_result = FAIL ;
+    
+    // run tests
+    for (int i = 0; i < 10; i++) {
+      return_value = gearsDrawMathNine(input2[i]);
+      actual_output[i] = return_value;
+    }
+
+    // compare values
+    for (int i = 0; i < 10; i++) {
+      if (expected_output2[i] == actual_output[i]) {
+	test_result = PASS;
+      }
+      else {
+	test_result = FAIL;
+	break;
+      }
+    }
+    
+    // emit result
+    if (test_result == FAIL)
+      mb_core_message("---unit test gearsDrawMathNine failed---");
+    if (test_result == PASS)
+      mb_core_message("---unit test gearsDrawMathNine passed---");
+
+    // ------------------------------------------------------------------------
+    // test 3 setup
+    GLfloat input3[10] = {0.0, 1.0, 2.0, 3.0, 4.5, 5.9, -1.1, -2.3, -3.0, -4.4};
+    GLfloat expected_output3[10] = {-25, -27, -29, -31, -34, -36.7999992, -22.7999992, -20.3999996, -19, -16.2000008};
+
+    // initialize test result
+    test_result = FAIL ;
+    
+    // run tests
+    for (int i = 0; i < 10; i++) {
+      return_value = gearsDrawMathTwentyFive(input3[i]);
+      actual_output[i] = return_value;
+    }
+
+    // compare values
+    for (int i = 0; i < 10; i++) {
+      if (expected_output3[i] == actual_output[i]) {
+	test_result = PASS;
+      }
+      else {
+	test_result = FAIL;
+	break;
+      }
+    }
+    
+    // emit result
+    if (test_result == FAIL)
+      mb_core_message("---unit test gearsDrawMathTwentyFive failed---");
+    if (test_result == PASS)
+      mb_core_message("---unit test gearsDrawMathTwentyFive passed---");
+    // ------------------------------------------------------------------------
+    
+
+    // emit end of testing message
+    mb_core_message("---end testing---");    
+
+    // halt the vp after emitting message.
+    mb_core_message("---halting vp---");
+    mb_stop(0);
+#endif
 }
 
